0cf2ec2ce16460e3bab0aa26a4ab5720b49540fd
Connor Shea <connor.james.shea@gmail.com>
2025-11-11 19:31:25 -0700

    Simplify eslint more

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,16 @@
-import js from '@eslint/js';
 import { globalIgnores } from 'eslint/config';
 import react from 'eslint-plugin-react';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export const baseConfig = [
-  js.configs.recommended,
   {
     linterOptions: {
       reportUnusedDisableDirectives: 'error',
       reportUnusedInlineConfigs: 'error',
     },
     rules: {
-      'consistent-return': 'off',
-      'dot-notation': 'off',
-      eqeqeq: "off",
-      'no-console': "off",
-      'no-empty': "off",
-      'no-restricted-properties': "off",
-      'no-unused-expressions': 'off',
       'no-unused-vars': 'off',
-      "no-undef": "off",
     },
   },
 ];
@@ -28,20 +18,7 @@ export const baseConfig = [
 export default tseslint.config([
   baseConfig,
   globalIgnores([
-    'build/**/*',
-    'coverage/**/*',
-    'db/**/*',
-    'lib/**/*',
-    'log/**/*',
     'node_modules/**/*',
-    'public/**/*',
-    '!public/embed.js',
-    'spec/**/*',
-    'tmp/**/*',
-    'vendor/**/*',
-    'streaming/**/*',
-    '.bundle/**/*',
-    'storybook-static/**/*',
   ]),
   react.configs.flat.recommended,
   react.configs.flat['jsx-runtime'],
